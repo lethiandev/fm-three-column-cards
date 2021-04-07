42998e42a211bc75bfd806e95b1bdc9a3007923e
Konrad Nowakowski <konrad.x92@gmail.com>
2021-04-07 21:24:16 +0100

    Fix public path configuration for github pages

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+const { defineConfig } = require('@vue/cli-service')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = defineConfig({
+  publicPath: isProduction ? '/fm-three-column-cards/' : '/',
+})
