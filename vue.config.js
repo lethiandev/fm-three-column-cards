5164a3405228a8b2efee69356c2552ce476d13fa
Konrad Nowakowski <konrad.x92@gmail.com>
2021-04-08 00:12:52 +0100

    Fix css modules configuration

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,20 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = defineConfig({
   publicPath: isProduction ? '/fm-three-column-cards/' : '/',
+  chainWebpack: config => {
+    // Fix scss modules configuration
+    config.module
+      .rule('scss')
+      .oneOf('normal-modules')
+      .use('css-loader')
+      .tap(options => {
+        options.modules = {
+          localIdentName: '[name]_[local]_[hash:base64:5]',
+          exportLocalsConvention: 'camelCase',
+          auto: true,
+        }
+
+        return options
+      })
+  },
 })
