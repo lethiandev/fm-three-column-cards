9624c82339da05d0de7d8b3cf7870e822e6c2410
Konrad Nowakowski <konrad.x92@gmail.com>
2021-04-07 18:06:54 +0100

    Fix home page buttons counting

--- tests/unit/HomePage.spec.ts
+++ tests/unit/HomePage.spec.ts
@@ -3,8 +3,8 @@ import HomePage from '@/pages/HomePage'
 import VButton from '@/components/VButton'
 
 describe('HomePage.tsx', () => {
-  it('should display button', () => {
+  it('should display three buttons', () => {
     const wrapper = mount(HomePage)
-    expect(wrapper.findAllComponents(VButton)).toHaveLength(1)
+    expect(wrapper.findAllComponents(VButton)).toHaveLength(3)
   })
 })
