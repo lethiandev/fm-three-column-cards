4a7d2c20f843e57800fb02589536996c7eddaafb
Konrad Nowakowski <konrad.x92@gmail.com>
2021-04-07 14:49:29 +0100

    Add card component to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,16 @@
-import VButton from '@/components/VButton'
 import { defineComponent } from 'vue'
+import VButton from '@/components/VButton'
+import VCard from '@/components/VCard'
 
 export default defineComponent({
   name: 'Home',
   setup() {
     return () => (
       <main>
-        <h1>Home page</h1>
-        <VButton>Hello, world!</VButton>
+        <VCard variant="first">
+          <h1>Home page</h1>
+          <VButton>Hello, world!</VButton>
+        </VCard>
       </main>
     )
   },
