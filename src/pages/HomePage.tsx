e5419e9e98b7871f8b084c057ac4e064550ca809
Konrad Nowakowski <konrad.x92@gmail.com>
2021-04-07 17:56:38 +0100

    Add columns align to center

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,19 +8,21 @@ export default defineComponent({
   setup() {
     return () => (
       <main class={styles.main}>
-        <div class={styles.columns}>
-          <VCard variant="first">
-            <h1>Home page</h1>
-            <VButton variant="first">Hello, world!</VButton>
-          </VCard>
-          <VCard variant="second">
-            <h1>Home page</h1>
-            <VButton variant="second">Hello, world!</VButton>
-          </VCard>
-          <VCard variant="third">
-            <h1>Home page</h1>
-            <VButton variant="third">Hello, world!</VButton>
-          </VCard>
+        <div class={styles.container}>
+          <div class={styles.columns}>
+            <VCard variant="first">
+              <h1>Home page</h1>
+              <VButton variant="first">Hello, world!</VButton>
+            </VCard>
+            <VCard variant="second">
+              <h1>Home page</h1>
+              <VButton variant="second">Hello, world!</VButton>
+            </VCard>
+            <VCard variant="third">
+              <h1>Home page</h1>
+              <VButton variant="third">Hello, world!</VButton>
+            </VCard>
+          </div>
         </div>
       </main>
     )
