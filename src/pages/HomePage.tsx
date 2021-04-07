81a3ed2e7e3c5a7b2faa910264f01bdca00090e0
Konrad Nowakowski <konrad.x92@gmail.com>
2021-04-07 17:32:53 +0100

    Add basic grid layout module

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,27 @@
 import { defineComponent } from 'vue'
 import VButton from '@/components/VButton'
 import VCard from '@/components/VCard'
+import styles from '@/scss/layout.module.scss'
 
 export default defineComponent({
   name: 'Home',
   setup() {
     return () => (
-      <main>
-        <VCard variant="first">
-          <h1>Home page</h1>
-          <VButton variant="first">Hello, world!</VButton>
-        </VCard>
+      <main class={styles.container}>
+        <div class={styles.columns}>
+          <VCard variant="first">
+            <h1>Home page</h1>
+            <VButton variant="first">Hello, world!</VButton>
+          </VCard>
+          <VCard variant="second">
+            <h1>Home page</h1>
+            <VButton variant="second">Hello, world!</VButton>
+          </VCard>
+          <VCard variant="third">
+            <h1>Home page</h1>
+            <VButton variant="third">Hello, world!</VButton>
+          </VCard>
+        </div>
       </main>
     )
   },
